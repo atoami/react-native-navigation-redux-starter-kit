df0ca09fc1607d84867a5f0b5f8431f448f19486
bdavid68 <b.david6008@gmail.com>
2017-10-14 03:34:32 +1000

    minor updates

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 import React, { PureComponent } from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import { connect } from 'react-redux';
-import { MainScene, DashboardScene } from 'AppScenes';
+import { LoginScene, DashboardScene } from 'AppScenes';
 import { Provider } from 'AppRedux';
 import createStore from './src/redux/store/store';
 
@@ -17,7 +17,7 @@ export default class App extends PureComponent {
       <Provider store={store}>
         <RouterWithRedux showNavigationBar={true}>
           <Scene key="root">
-            <Scene key="main" title="Main" component={MainScene} initial={true} />
+            <Scene key="main" title="Main" component={LoginScene} initial={true} hideNavBar />
             <Scene key="dashboard" title="Dashboard" component={DashboardScene} />
           </Scene>
         </RouterWithRedux>
--- src/configs/index.js
+++ src/configs/index.js
@@ -3,16 +3,7 @@
  */
 
 module.exports = {
-  firebaseConfig: {
-    apiKey: 'AIzaSyBxy0AdUbnrs3ppbHdJ5W2wBOqdo_zk7G4',
-    authDomain: 'tieit-kanban-tasks-app.firebaseapp.com',
-    databaseURL: 'https://tieit-kanban-tasks-app.firebaseio.com',
-    projectId: 'tieit-kanban-tasks-app',
-    storageBucket: 'tieit-kanban-tasks-app.appspot.com',
-    messagingSenderId: '117046515520'
-  },
   apiConfig: {
-    auth_endpoint: 'http://localhost:8000/',
     api_endpoint: 'http://localhost:8000/api/'
   }
 };
