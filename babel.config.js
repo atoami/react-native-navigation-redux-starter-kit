317637e68c23f5c080279454cedb535800d9748b
wagng <w.gang6008@gmail.com>
2018-12-26 11:23:02 +1000

    Fix bundling bug

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,7 @@
-// {
-//   "presets": ["env", "module:metro-react-native-babel-preset"],
-//   "plugins": [
-//     ["@babel/plugin-proposal-decorators", { "legacy": true }],
-//     "@babel/plugin-syntax-export-namespace-from",
-//     "transform-export-extensions"
-//   ]
-// }
 'use strict'
 
 module.exports = {
-  presets: ["env", "@babel/preset-env", "module:metro-react-native-babel-preset"],
+  presets: ["module:metro-react-native-babel-preset"],
   plugins: [
     "@babel/plugin-transform-modules-commonjs",
     ["@babel/plugin-proposal-decorators", { "legacy": true }],
